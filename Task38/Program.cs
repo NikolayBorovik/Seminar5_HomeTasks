44b13423679352c89151e4b7f1ca8866432fec19
Nikolay Borovik <nik.Borovik@bk.ru>
2023-02-10 16:19:37 +1000

    Task38 done

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] array = GetArray(7);
+
+double[] GetArray (int size)
+{
+    double[] collection = new double[size];
+    for (int i = 0; i < size; i++) 
+    {
+    Random rand = new Random();
+    double min = 1;
+    double max = 10;
+    double range = max - min;
+    for (int j = 0; j < 10; j++)
+        {
+            double sample = rand.NextDouble();
+            double scaled = (sample * range) + min;
+            scaled = Math.Round(scaled, 1);
+            collection[i] = scaled;
+        }
+    }
+    return collection;
+}
+
+System.Console.WriteLine(String.Join(" ", array));
+
+double MaxMinNumDiff (double[] collection)
+{
+    double max = 0;
+    double min = collection[0];
+    foreach (var item in collection)
+    {
+        if (item > max) max = item;
+        else if(item<min) min = item;
+    }
+    double diff = max - min;
+    diff = Math.Round(diff,1);
+    return diff;
+}
+
+double result = MaxMinNumDiff(array);
+System.Console.WriteLine(result);
